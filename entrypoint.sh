587a7db04a8ef8d31bf0bde7d7cb28e949df3e61
Benjamin Jonard <benjamin.jonard@sooyoos.com>
2022-09-30 09:34:19 +0200

    Add refresh cached values command to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,41 +2,45 @@
 
 set -e
 
-echo "**** Make sure the /conf and /uploads folders exist ****"
+echo "**** 1/13 - Make sure the /conf and /uploads folders exist ****"
 [ ! -f /conf ] && \
 	mkdir -p /conf
 [ ! -f /uploads ] && \
 	mkdir -p /uploads
 
-echo "**** Create the symbolic link for the /uploads folder ****"
+echo "**** 2/13 - Create the symbolic link for the /uploads folder ****"
 [ ! -L /var/www/koillection/public/uploads ] && \
 	cp -r /var/www/koillection/public/uploads/. /uploads && \
 	rm -r /var/www/koillection/public/uploads && \
 	ln -s /uploads /var/www/koillection/public/uploads
 
-echo "**** Copy the .env to /conf ****"
+echo "**** 3/13 - Copy the .env to /conf ****"
 [ ! -e /conf/.env.local ] && \
 	touch /conf/.env.local
 [ ! -L /var/www/koillection/.env.local ] && \
 	ln -s /conf/.env.local /var/www/koillection/.env.local
 
-echo "**** Inject .env values ****"
+echo "**** 4/13 - Inject .env values ****"
 	/inject.sh
 
-echo "**** Configure https ****"
+echo "**** 5/13 - Configure https ****"
 if ! grep -q "session.cookie_secure=" /etc/php/8.1/fpm/conf.d/php.ini; then
     echo "session.cookie_secure=${HTTPS_ENABLED}" >> /etc/php/8.1/fpm/conf.d/php.ini
 fi
 
-echo "**** Migrate the database ****"
+echo "**** 6/13 - Migrate the database ****"
 cd /var/www/koillection && \
 php bin/console doctrine:migration:migrate --no-interaction --allow-no-migration --env=prod
 
-echo "**** Create API keys ****"
+echo "**** 7/13 - Refresh cached values ****"
+cd /var/www/koillection && \
+php bin/console app:refresh-cached-values --env=prod
+
+echo "**** 8/13 - Create API keys ****"
 cd /var/www/koillection && \
 php bin/console lexik:jwt:generate-keypair --overwrite --env=prod
 
-echo "**** Create user and use PUID/PGID ****"
+echo "**** 9/13 - Create user and use PUID/PGID ****"
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 if [ ! "$(id -u "$USER")" -eq "$PUID" ]; then usermod -o -u "$PUID" "$USER" ; fi
@@ -44,18 +48,18 @@ if [ ! "$(id -g "$USER")" -eq "$PGID" ]; then groupmod -o -g "$PGID" "$USER" ; f
 echo -e " \tUser UID :\t$(id -u "$USER")"
 echo -e " \tUser GID :\t$(id -g "$USER")"
 
-echo "**** Set Permissions ****" && \
+echo "**** 10/13 - Set Permissions ****" && \
 find /uploads -type d \( ! -user "$USER" -o ! -group "$USER" \) -exec chown -R "$USER":"$USER" \{\} \;
 find /conf/.env.local /uploads \( ! -user "$USER" -o ! -group "$USER" \) -exec chown "$USER":"$USER" \{\} \;
 usermod -a -G "$USER" www-data
 find /uploads -type d \( ! -perm -ug+w -o ! -perm -ugo+rX \) -exec chmod -R ug+w,ugo+rX \{\} \;
 find /conf/.env.local /uploads \( ! -perm -ug+w -o ! -perm -ugo+rX \) -exec chmod ug+w,ugo+rX \{\} \;
 
-echo "**** Create nginx log files ****" && \
+echo "**** 11/13 - Create nginx log files ****" && \
 mkdir -p /logs/nginx
 chown -R "$USER":"$USER" /logs/nginx
 
-echo "**** Create symfony log files ****" && \
+echo "**** 12/13 - Create symfony log files ****" && \
 [ ! -f /var/www/koillection/var/log ] && \
 	mkdir -p /var/www/koillection/var/log
 
@@ -64,6 +68,8 @@ echo "**** Create symfony log files ****" && \
 
 chown -R www-data:www-data /var/www/koillection/var
 
-echo "**** Setup complete, starting the server. ****"
+echo "**** 13/13 - Setup complete, starting the server. ****"
 php-fpm8.1
 exec $@
+
+echo "**** All done ****"
